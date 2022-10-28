0270ee7f912a89a7fa7c378c9c8bfc6fb6738df5
Lilia <liliamaria.lys@gmail.com>
2022-10-27 23:32:42 -0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,48 @@
+import Food from  './Food'
+
+function App() {
+  const foods  = [{
+    name:  'Capuccino com Nutella',
+    image: 'imgs/capNutella.jpg'
+  },
+  {
+    id: 2,
+    name: 'Capuccino Tradicional',
+    image: 'imgs/capuccino.jpg',
+  },
+  {
+    id: 3,
+    name: 'Café mocca',
+    image: 'imgs/mocca.jpg',
+  },
+  {
+    id: 4,
+    name: 'Smoothie de Morango',
+    image: 'imgs/smoothie.jpg',
+  },]
+ 
+  return (
+    <div className="container">
+    <h1 className="mt-5 text center">Menu</h1>
+    <div className="text-right">
+    <button
+    type="button"
+    className="btn btn-secondary rounded-circle mr-4 font-weight-bold"
+   data-toggle="modal"
+   data="#formFoodModal"
+   // onClick="loadFormCreateFood()"
+  > + </button>
+    </div>
+    <section className="card-deck my-3">
+     {foods.map((food) =>{ return <Food food={food} key={food.id} /> })}
+          
+      </section>
+    </div>
+      
+
+  
+    
+  );
+}
+
+export default App;
---
+++ src/models/foods.js
@@ -0,0 +1,24 @@
+const foods = [
+  {
+    id: 1,
+    name: 'Capuccino com Nutella',
+    image: 'imgs/capNutella.jpg',
+  },
+  {
+    id: 2,
+    name: 'Capuccino Tradicional',
+    image: 'imgs/capuccino.jpg',
+  },
+  {
+    id: 3,
+    name: 'Café mocca',
+    image: 'imgs/mocca.jpg',
+  },
+  {
+    id: 4,
+    name: 'Smoothie de Morango',
+    image: 'imgs/smoothie.jpg',
+  },
+];
+
+export default foods;
